7b88d88ae3da22b6f36769d8cb05646dafbf9470
jose antonio <jsancesc@gmail.com>
2022-12-02 23:25:00 +0100

    juego5

---
+++ RgbConversion.py
@@ -0,0 +1,20 @@
+cadena =" "
+def rgb(r, g, b):
+  if r > 255:
+    cadena.__add__("FF")
+  elif 0 < r < 255 :
+    cadena.__add__(r/2)
+  else:
+    cadena.__add__("00")
+  if g > 255:
+    cadena.__add__("FF")
+  elif 0 < g < 255 :
+    cadena.__add__(g/2)
+  else:
+    cadena.__add__("00")
+  if b > 255:
+    cadena.__add__("FF")
+  elif 0 < b < 255 :
+    cadena.__add__(b/2)
+  else:
+    cadena.__add__("00")
